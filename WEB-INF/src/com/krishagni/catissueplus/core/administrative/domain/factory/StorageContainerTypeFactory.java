5a891da163823765dfdc52c45befb18e3c273290
nasim0786 <md.nasim@krishagni.com>
2016-02-16 13:01:29 +0530

    ContainType: Removed all StorageContainerType name files.

--- WEB-INF/src/com/krishagni/catissueplus/core/administrative/domain/factory/StorageContainerTypeFactory.java
+++
@@ -1,9 +0,0 @@
-package com.krishagni.catissueplus.core.administrative.domain.factory;
-
-import com.krishagni.catissueplus.core.administrative.domain.StorageContainerType;
-import com.krishagni.catissueplus.core.administrative.events.StorageContainerTypeDetail;
-
-public interface StorageContainerTypeFactory {
-	public StorageContainerType createStorageContainerType(StorageContainerTypeDetail detail);
-	
-}
